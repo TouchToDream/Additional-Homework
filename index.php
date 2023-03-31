a6c5e4ddc61db5a3c934f6daad081bd550c0e525
TouchToDream <s1lence10@hotmail.com>
2023-03-31 16:56:50 +0300

    first part

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+$ignoreFolders = ['folder 5'];
+
+function scanDirectory($dir, $ignoreFolders)
+{
+    $result = [];
+    $files = scandir($dir);
+    foreach ($files as $value) {
+        if ($value != '.' && $value != '..' && !in_array($value, $ignoreFolders) && $value != 'folder 2') {
+            $path = $dir . '/' . $value;
+            if (is_dir($path)) {
+                $result[] = ['is_dir' => true, 'path' => $path, 'name' => $value];
+                $result = array_merge($result, scanDirectory($path, $ignoreFolders));
+            } else {
+                $result[] = ['is_dir' => false, 'path' => $path, 'name' => $value];
+            }
+        }
+    }
+    return $result;
+}
+
+$results = scanDirectory('.', $ignoreFolders);
+foreach ($results as $file) {
+    echo $file['path'] . ' - ' . ($file['is_dir'] 
+        ? '<span style="color:blue;">Directory</span>' 
+        : '<span style="color:green;">File</span>') . '</br>';
+}
+echo '<pre>';
+var_dump($results);
+echo '</pre>';
