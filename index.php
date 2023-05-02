bd77d157df571f77fc8550ed30374b9a3655d755
TouchToDream <s1lence10@hotmail.com>
2023-05-02 18:34:33 +0300

    update function

--- index.php
+++ index.php
@@ -2,10 +2,16 @@
 
 $ignoreFolders = ['folder 5', '.git'];
 
-class Item
+interface Deletable
+{
+    public function delete();
+}
+
+abstract class Item
 {
     public $path;
     public $name;
+
     public function __construct(string $path, string $name)
     {
         $this->path = $path;
@@ -13,47 +19,53 @@ class Item
     }
 }
 
-class File extends Item
+class File extends Item implements Deletable
 {
     public function __construct(string $path, string $name)
     {
         parent::__construct($path, $name);
     }
+
+    public function delete()
+    {
+    }
 }
 
 class Folder extends Item
 {
-    public $is_dir;
-    public function __construct(bool $is_dir, string $path, string $name)
+    public function __construct(string $path, string $name)
     {
         parent::__construct($path, $name);
-        $this->is_dir = $is_dir;
     }
-}
 
-
-function scanDirectory($dir, $ignoreFolders)
-{
-    $result = [];
-    $files = scandir($dir);
-    foreach ($files as $value) {
-        if ($value !== '.' && $value !== '..' && !in_array($value, $ignoreFolders)) {
-            $path = $dir . '/' . $value;
-            if (is_dir($path)) {
-                $result[] = new Folder(true, $path, $value);
-                $result = array_merge($result, scanDirectory($path, $ignoreFolders));
-            } else {
-                $result[] = new File($path, $value);
+    public static function scanDirectory(string $dir, array $ignoreFolders)
+    {
+        $result = [];
+        $files = scandir($dir);
+        foreach ($files as $value) {
+            if ($value !== '.' && $value !== '..' && !in_array($value, $ignoreFolders)) {
+                $path = $dir . '/' . $value;
+                if (is_dir($path)) {
+                    $result[] = new Folder($path, $value);
+                    $result = array_merge($result, self::scanDirectory($path, $ignoreFolders));
+                } else {
+                    $result[] = new File($path, $value);
+                }
             }
         }
+        return $result;
     }
-    return $result;
 }
 
+$results = Folder::scanDirectory('.', $ignoreFolders);
 
-$results = scanDirectory('.', $ignoreFolders);
 foreach ($results as $file) {
-    echo $file->path . ' - ' . ($file->is_dir
+    $objectName = get_class($file);
+    echo $file->path . ' - ' . ($objectName === 'Folder'
         ? '<span style="color:blue;">Directory</span>'
         : '<span style="color:green;">File</span>') . '</br>';
 }
+
+echo '<pre>';
+var_dump($results);
+echo '</pre>';
